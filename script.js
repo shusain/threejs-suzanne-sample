a2d6ae5dc7df5d9b19e2d5acc160c57ba0de9529
Shaun Husain <shaun.husain@gmail.com>
2023-12-30 04:36:15 -0600

    now with trackball controls

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+import { TrackballControls } from 'three/addons/controls/TrackballControls.js';
 
 const width = window.innerWidth, height = window.innerHeight;
 
@@ -11,10 +12,10 @@ camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+// const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+// const material = new THREE.MeshNormalMaterial();
 
-const mesh = new THREE.Mesh( geometry, material );
+// const mesh = new THREE.Mesh( geometry, material );
 // scene.add( mesh );
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -22,18 +23,25 @@ renderer.setSize( width, height );
 renderer.setAnimationLoop( animation );
 document.body.appendChild( renderer.domElement );
 
-// animation
+
+const controls = new TrackballControls( camera, renderer.domElement );
+controls.minDistance = 1;
+controls.maxDistance = 10;
+controls.rotateSpeed= 2
+
 let loadedScene = null
 
+// animation
 function animation( time ) {
+  controls.update()
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+	// mesh.rotation.x = time / 2000;
+	// mesh.rotation.y = time / 1000;
 
-  if(loadedScene){
-    loadedScene.children[0].rotation.x = time / 2000;
-    loadedScene.children[0].rotation.y = time / 500;
-  }
+  // if(loadedScene){
+  //   loadedScene.children[0].rotation.x = time / 2000;
+  //   loadedScene.children[0].rotation.y = time / 500;
+  // }
 
 	renderer.render( scene, camera );
 }
